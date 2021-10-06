af9288d428521c6d29768b8522f8220b2860eed9
kojinghong <90750570+kojinghong@users.noreply.github.com>
2021-10-06 16:03:06 +0800

    20211006

--- Elevator_3.js
+++ Elevator_3.js
@@ -1,18 +1,22 @@
 const readline = require('readline-sync')
 
 let top = 10 //電梯頂層
-let bottom = 1 //電梯底層
+let bottom = 0 //電梯底層
+let floorName = ['B3', 'B2', 'B1', '1', '2', '2M', '3', '5', '6', '7', '8']
 let floor = 5 //目前樓層
-let targetFloor //到達樓層
+let targetFloor //到達樓層 //B2 -> [1]
 
 while (true) { //無窮迴圈
     //輸入欲達樓層
-    targetFloor = readline.question('目前在' + floor + '樓，請問要去幾樓？')
+    targetFloor = readline.question('目前在' + floorName[floor] + '樓，請問要去幾樓？')
 
     //判斷輸入樓層是否合理
+    //從floorName.indexOf()取出索引值，若為-1代表無此樓
+    targetFloor = targetFloor.toUpperCase() //把輸入的小寫轉成大寫
+    targetFloor = floorName.indexOf(targetFloor) //索引陣列樓層
     targetFloor = parseInt(targetFloor) //parseInt轉成整數
-    if (isNaN(targetFloor) || targetFloor < bottom || targetFloor > top) {
-        console.log('請輸入介於' + bottom + '至' + top + '之間的整數')
+    if (targetFloor < 0) {
+        console.log('請輸入介於' + floorName.toString() + '之間的整數') //toString 輸出所有陣列內容
         continue
     }
     if (targetFloor == floor) {
@@ -22,20 +26,18 @@ while (true) { //無窮迴圈
         //蝘餃����餅０��單炬���璅�撅�
         //floor 蝘餃�� targetfloor
         if (floor > targetFloor) { //Down
-            console.log('��餅０敺�銝�......');
             //��寧�� for(���憪���潭��隞嗅�斗��:蝝臬��皜�)
-            //while(targetFloor <currentFloor)f
-            //for(var i=currentFloor; i>targetFloor;i--)f
-            for (; targetFloor < currentFloor; currentFloor--)
+            //while(targetFloor <currentFloor)
+            //for(var i=currentFloor; i>targetFloor;i--)
+            for (; targetFloor < floor; floor--)
                 floor = floor - 1
             // currentFloor-=1
             // currentFloor--
-            console.log("電梯在" + currentFloor + "樓")
+            console.log("電梯在" + floorName[floor] + "樓")
         } else { //up
-            console.log('��餅０敺�銝�......')
             while (targetFloor > floor) {
                 floor = floor + 1
-                console.log('電梯在' + floor + '樓')
+                console.log('電梯在' + floorName[floor] + '樓')
             }
         }
     }
--- Hello.js
+++ Hello.js
@@ -1,30 +1,30 @@
 //VS PPT: https://drive.google.com/file/d/1pjOZp8Xp6Ghb31Nfnj0YRsVwAzMMyhCq/view?usp=sharing
 //JS PPT: https://drive.google.com/open?id=0BwN5VWV_C7-5ZnE3d045YWZJb00
-//Javascript 變數的使用
+//Javascript ���嚙踝蕭嚙質��嚙踝蕭��輯撒嚙踝蕭
 
 var weight,height
 
 
 
 const readline = require('readline-sync')
-weight = readline.question('請輸入您的體重(kg)?')
+weight = readline.question('��ｇ蕭���閰剁蕭鈭歹蕭��踝蕭嚙賣��嚙踝蕭嚙踝蕭(kg)?')
 if (weight>=10 && weight<=200){
-    height = readline.question('請輸入您的身高(cm)?')
+    height = readline.question('��ｇ蕭���閰剁蕭鈭歹蕭��踝蕭嚙賡�冽��嚙踝蕭(cm)?')
     if (height>=50 && height<=220){
         var bmi = weight/((height/100)**2)
         console.log("Hello! Your BMI value is "+ bmi)
     } else {
-        console.log("身高輸入錯誤，請重新輸入")
+        console.log("��冽��嚙踝蕭���閰剁蕭隞選蕭��芰�����嚙賡�ｇ蕭嚙踝蕭嚙踝蕭嚙賢�����嚙踝蕭嚙�")
     }
 } else {
-    console.log("體重輸入錯誤，請重新輸入")
+    console.log("���嚙踝蕭嚙踝蕭���閰剁蕭隞選蕭��芰�����嚙賡�ｇ蕭嚙踝蕭嚙踝蕭嚙賢�����嚙踝蕭嚙�")
 }
 
 
-//使用第三方套件 readline-sync (待使用者輸入資料後程式再繼續執行) 
-//NodeJS 套件管理程式為 npm，需打開終端機輸入指令
-//安裝 readline-sync 套件指令: npm install readline-sync
-//另可透過package.json指定所有相關套件後 npm install 即可安裝所有套件
+//��輯撒嚙賢�賣斯���嚙踝蕭嚙賢音嚙踝蕭���嚙� readline-sync (��綽蕭��輯撒嚙賢�鳴蕭嚙賡��閰剁蕭鈭伐蕭嚙踝蕭嚙踝蕭��綽蕭���嚙賣��嚙踝蕭嚙踝蕭���瞏�嚙踝蕭嚙踝蕭���嚙踝蕭) 
+//NodeJS ���嚙賡����交��嚙踝蕭嚙質��嚙賣��嚙踝蕭嚙踝蕭 npm���嚙踝蕭嚙踝蕭嚙踝蕭嚙踝蕭嚙踝蕭��荔蕭��∟��嚙踝蕭���閰剁蕭鈭歹蕭嚙賡��嚙�
+//��堆蕭���嚙� readline-sync ���嚙賡�����嚙踝蕭���嚙�: npm install readline-sync
+//嚙踝蕭銵�嚙質�恬蕭嚙踝蕭嚙踝蕭package.json嚙踝蕭嚙賣�堆蕭嚙踝蕭嚙踝蕭嚙踝蕭嚙踝蕭鞈�嚙踝蕭���嚙賡�����嚙踝蕭 npm install 嚙踝蕭��喉蕭��穿蕭嚙賡��嚙踝蕭嚙踝蕭嚙踝蕭嚙賣��嚙賡��嚙�
 
 
 //not only bmi value, but also judgement
