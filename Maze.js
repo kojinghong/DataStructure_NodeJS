e6b0362d52cabd888d539a5d38987b97023df255
kfhwang <kfhwang@gm.nutc.edu.tw>
2021-11-03 14:33:17 +0800

    20211103

--- Maze.js
+++ Maze.js
@@ -11,4 +11,41 @@ var  MAZE= [
     [1,1,1,1,1,1,1,1,1,1,1,1]
    ]; 
 
+var currentRow=1, currentCol=1;//start point
+var exitRow=8, exitCol=10;
+
+class Point{
+    constructor(_row, _col){
+        this.row = _row;
+        this.col = _col;
+    }
+    print(){
+        return "("+this.row+", "+this.col+")";
+    }
+}
    
+//Stack
+var stack=[];
+stack.push(new Point(currentRow, currentCol)); //push start point to stack
+//try to find exit
+//!(currentRow==exitRow && currentCol==exitCol)
+while(currentRow != exitRow || currentCol!=exitCol){
+    
+    //right
+    if(MAZE[currentRow][currentCol+1] == 0){
+        MAZE[currentRow][currentCol+1] = 2;
+        stack.push(new Point(currentRow, currentCol+1)) 
+        currentCol+=1;
+        continue;
+    }
+    //up
+
+    //left
+
+    //down
+
+
+    //terminal
+
+
+}
